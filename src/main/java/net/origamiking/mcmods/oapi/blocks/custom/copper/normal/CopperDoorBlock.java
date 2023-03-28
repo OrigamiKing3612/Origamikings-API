0152b1ebe5bd2436115e00c2a03269269cfd9814
OrigamiKing3612 <origamiking3612@gmail.com>
2023-03-28 12:10:35 -0500

    Downgrade to 1.19.2

--- src/main/java/net/origamiking/mcmods/oapi/blocks/custom/copper/normal/CopperDoorBlock.java
+++ src/main/java/net/origamiking/mcmods/oapi/blocks/custom/copper/normal/CopperDoorBlock.java
@@ -1,13 +1,12 @@
 package net.origamiking.mcmods.oapi.blocks.custom.copper.normal;
 
-import net.minecraft.block.BlockSetType;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.Oxidizable;
 
 public class CopperDoorBlock extends DoorBlock {
     public final Oxidizable.OxidationLevel oxidizationLevel;
     public CopperDoorBlock(Oxidizable.OxidationLevel oxidizationLevel, Settings settings) {
-        super(settings, BlockSetType.IRON);
+        super(settings);
         this.oxidizationLevel = oxidizationLevel;
     }
 
--- src/main/java/net/origamiking/mcmods/oapi/blocks/custom/copper/normal/CopperPresurePlateBlock.java
+++ src/main/java/net/origamiking/mcmods/oapi/blocks/custom/copper/normal/CopperPresurePlateBlock.java
@@ -1,13 +1,12 @@
 package net.origamiking.mcmods.oapi.blocks.custom.copper.normal;
 
-import net.minecraft.block.BlockSetType;
 import net.minecraft.block.Oxidizable;
 import net.minecraft.block.PressurePlateBlock;
 
 public class CopperPresurePlateBlock extends PressurePlateBlock {
     public final Oxidizable.OxidationLevel oxidizationLevel;
     public CopperPresurePlateBlock(Oxidizable.OxidationLevel oxidizationLevel, Settings settings) {
-        super(ActivationRule.EVERYTHING, settings, BlockSetType.IRON);
+        super(ActivationRule.EVERYTHING, settings);
         this.oxidizationLevel = oxidizationLevel;
     }
 
