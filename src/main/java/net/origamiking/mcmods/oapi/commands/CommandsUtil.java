2da3e4f0d78816d6935109f5833e1ef363080fad
OrigamiKing3612 <origamiking3612@gmail.com>
2023-03-30 12:22:17 -0500

    Downgrade to 1.18.2

--- src/main/java/net/origamiking/mcmods/oapi/commands/CommandsUtil.java
+++ src/main/java/net/origamiking/mcmods/oapi/commands/CommandsUtil.java
@@ -1,9 +1,11 @@
 package net.origamiking.mcmods.oapi.commands;
 
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.Text;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.*;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -12,19 +14,22 @@ public class CommandsUtil {
      * <p>CommandsUtil.makeVersionCommand(MOD_ID, VERSION);</p>
      */
     public static void makeVersionCommand(String id, String version) {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal(id + "-version")
-                .executes(context -> {
-                    context.getSource().sendMessage(Text.of(version));
-                    return 1;
-                })));
+        CommandRegistrationCallback.EVENT.register((dispatcher, commandSender) -> {
+            LiteralArgumentBuilder<ServerCommandSource> builder = CommandManager.literal(id + "-version")
+                    .executes((CommandContext<ServerCommandSource> context) -> {
+                        context.getSource().sendFeedback(Text.of(version), false);
+                        return 1;
+                    });
+            dispatcher.register(builder);
+        });
     }
     /** To use make this
      * <p>CommandsUtil.simpleStringReturnCommand("Command-Name", "Text to say");</p>
      */
     public static void simpleStringReturnCommand(String commandName, String text) {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal(commandName)
+        CommandRegistrationCallback.EVENT.register((dispatcher, commandSender) -> dispatcher.register(literal(commandName)
                 .executes(context -> {
-                    context.getSource().sendMessage(Text.of(text));
+                    context.getSource().sendFeedback(Text.of(text), false);
                     return 1;
                 })));
     }
@@ -33,13 +38,17 @@ public class CommandsUtil {
      * To use
      * <p>CommandsUtil.linkReturnCommand("Command-Name", "Link", "Hover-Text");</p>
      */
+
     public static void linkReturnCommand(String commandName, String link, String hoverText) {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(literal(commandName)
-                    .executes(context -> {
-                        context.getSource().sendMessage(Text.literal(link).styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link))).styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.of(hoverText)))));
+        CommandRegistrationCallback.EVENT.register((dispatcher, commandSender) -> {
+            LiteralArgumentBuilder<ServerCommandSource> builder = CommandManager.literal(commandName)
+                    .executes((CommandContext<ServerCommandSource> context) -> {
+                        Style style = Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link))
+                                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText(hoverText)));
+                        context.getSource().sendFeedback(new LiteralText(link).setStyle(style), false);
                         return 1;
-                    }));
+                    });
+            dispatcher.register(builder);
         });
     }
 }
