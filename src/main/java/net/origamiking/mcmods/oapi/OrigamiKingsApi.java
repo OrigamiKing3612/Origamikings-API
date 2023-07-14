ca4ab603943ee4a414285a69a6629772104ca96c
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-13 19:09:20 -0500

    Updating API again

--- src/main/java/net/origamiking/mcmods/oapi/OrigamiKingsApi.java
+++ src/main/java/net/origamiking/mcmods/oapi/OrigamiKingsApi.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 public class OrigamiKingsApi implements ModInitializer {
 	public static final String MOD_ID = "origamikings-api";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final String VERSION ="0.1.15-1.20.1";
+	public static final String VERSION ="0.1.16-1.20.1";
 
 	@Override
 	public void onInitialize() {
