9efd55c868521a5eb4c084bd386c8900e2ea1eae
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-16 09:03:01 -0500

    Added more Client Utils

--- src/main/java/net/origamiking/mcmods/oapi/client/ClientUtils.java
+++ src/main/java/net/origamiking/mcmods/oapi/client/ClientUtils.java
@@ -1,7 +1,9 @@
 package net.origamiking.mcmods.oapi.client;
 
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.client.color.world.FoliageColors;
 import net.minecraft.client.render.RenderLayer;
 
 public class ClientUtils {
@@ -13,5 +15,21 @@ public class ClientUtils {
     }
     public static void getCutoutMipped(Block block) {
         BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutoutMipped());
+    }
+    public static void defaultColorProviderRegistry(Block ... blocks) {
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> FoliageColors.getDefaultColor(), blocks);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> FoliageColors.getDefaultColor(), blocks);
+    }
+    public static void birchColorProviderRegistry(Block ... blocks) {
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> FoliageColors.getBirchColor(), blocks);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> FoliageColors.getBirchColor(), blocks);
+    }
+    public static void mangroveColorProviderRegistry(Block ... blocks) {
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> FoliageColors.getMangroveColor(), blocks);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> FoliageColors.getMangroveColor(), blocks);
+    }
+    public static void spruceColorProviderRegistry(Block ... blocks) {
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> FoliageColors.getSpruceColor(), blocks);
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> FoliageColors.getSpruceColor(), blocks);
     }
 }
