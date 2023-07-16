7dd48214662bf4c76db5dcec126db7a552292a3b
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-16 09:12:37 -0500

    Fixed bug

--- src/main/java/net/origamiking/mcmods/oapi/stats/StatUtils.java
+++ src/main/java/net/origamiking/mcmods/oapi/stats/StatUtils.java
@@ -8,7 +8,7 @@ import net.minecraft.util.Identifier;
 import static net.minecraft.stat.Stats.CUSTOM;
 
 public class StatUtils {
-    public static Identifier register(String modid, String id, StatFormatter formatter) {
+    public static Identifier registerStat(String modid, String id, StatFormatter formatter) {
         Identifier identifier = new Identifier(modid + ":" + id);
         Registry.register(Registries.CUSTOM_STAT, id, identifier);
         CUSTOM.getOrCreateStat(identifier, formatter);
