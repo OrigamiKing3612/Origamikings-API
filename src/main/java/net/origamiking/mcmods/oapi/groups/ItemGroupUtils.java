5a64989e33eb914a3c0c3f6cb70eed213aa64c84
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-24 15:00:29 -0500

    Fixed bug

--- src/main/java/net/origamiking/mcmods/oapi/groups/ItemGroupUtils.java
+++ src/main/java/net/origamiking/mcmods/oapi/groups/ItemGroupUtils.java
@@ -1,11 +1,12 @@
 package net.origamiking.mcmods.oapi.groups;
 
+import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ItemGroupUtils {
-    public static void itemGroup(String modid, String id) {
-        RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(modid, id));
+    public static RegistryKey<ItemGroup> itemGroup(String modid, String id) {
+        return RegistryKey.of(RegistryKeys.ITEM_GROUP, new Identifier(modid, id));
     }
 }
