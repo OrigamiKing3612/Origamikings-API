cd3343677e04285ac057d43168fe91ee0289a2e7
OrigamiKing3612 <origamiking3612@gmail.com>
2023-07-25 17:31:22 -0500

    Update Fabric-API

--- src/main/java/net/origamiking/mcmods/oapi/OrigamiKingsApi.java
+++ src/main/java/net/origamiking/mcmods/oapi/OrigamiKingsApi.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 public class OrigamiKingsApi implements ModInitializer {
 	public static final String MOD_ID = "origamikings-api";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final String VERSION ="0.1.24-1.20.1";
+	public static final String VERSION ="0.1.25-1.20.1";
 
 	@Override
 	public void onInitialize() {
