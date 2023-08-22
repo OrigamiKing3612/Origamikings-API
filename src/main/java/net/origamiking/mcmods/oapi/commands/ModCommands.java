2f5640f83e44f9e207b90f8b8862f7dad8c79727
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-22 12:34:31 -0500

    Update Commands

--- src/main/java/net/origamiking/mcmods/oapi/commands/ModCommands.java
+++ src/main/java/net/origamiking/mcmods/oapi/commands/ModCommands.java
@@ -1,11 +1,20 @@
 package net.origamiking.mcmods.oapi.commands;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
 import net.origamiking.mcmods.oapi.OrigamiKingsApi;
 
 public class ModCommands {
     public static void registerCommands() {
-        CommandsUtil.makeVersionCommand(OrigamiKingsApi.MOD_ID, OrigamiKingsApi.VERSION);
-        CommandsUtil.linkReturnCommand(OrigamiKingsApi.MOD_ID + "-website", "https://modrinth.com/mod/origamikings-api", "To Website");
-        CommandsUtil.linkReturnCommand(OrigamiKingsApi.MOD_ID + "-issues", "https://github.com/OrigamiKing3612/Origamikings-API/issues", "To Issues Page");
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            LiteralArgumentBuilder<ServerCommandSource> command = LiteralArgumentBuilder.literal(OrigamiKingsApi.MOD_ID);
+
+            CommandsUtil.versionCommand(command, OrigamiKingsApi.VERSION, OrigamiKingsApi.MOD_ID);
+            CommandsUtil.linkReturnCommand(command, "website", "https://modrinth.com/mod/origamikings-api", "To Website");
+            CommandsUtil.linkReturnCommand(command, "issues", "https://github.com/OrigamiKing3612/Origamikings-API/issues", "To Issues Page");
+
+            dispatcher.register(command);
+        });
     }
 }
