74be5b1e42daedc0e6479aad225174c614c1bdfa
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-22 12:21:44 -0500

    Added commands Methods

--- src/main/java/net/origamiking/mcmods/oapi/commands/CommandsUtil.java
+++ src/main/java/net/origamiking/mcmods/oapi/commands/CommandsUtil.java
@@ -1,6 +1,8 @@
 package net.origamiking.mcmods.oapi.commands;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
@@ -41,6 +43,26 @@ public class CommandsUtil {
                         return 1;
                     }));
         });
+    }
+
+    public static void linkReturnCommand(LiteralArgumentBuilder<ServerCommandSource> command, String commandName, String link, String hoverText) {
+        command.then(literal(commandName).executes(context -> {
+            context.getSource().sendMessage(Text.literal(link).styled(style -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link))).styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.of(hoverText)))));
+            return 1;
+        }));
+    }
+    public static void versionCommand(LiteralArgumentBuilder<ServerCommandSource> command, String version) {
+        command.then(literal("version").executes(context -> {
+            context.getSource().sendMessage(Text.of(version));
+            return 1;
+        }));
+    }
+    public static void simpleStringReturnCommand(LiteralArgumentBuilder<ServerCommandSource> command, String commandName, String text) {
+        command.then(literal(commandName)
+                .executes(context -> {
+                    context.getSource().sendMessage(Text.of(text));
+                    return 1;
+                }));
     }
 }
 
